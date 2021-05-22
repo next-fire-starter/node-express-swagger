be01e6658292887e30141b8f5181832b33ffaf45
next-fire-starter <next.fire.starter@gmail.com>
2021-05-23 00:34:24 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'node-express-swagger',
+      title: 'node-express-swagger Project',
       version: '1.0.0',
     },
   },
