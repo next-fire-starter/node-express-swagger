be4fe7e0b12e10f1b9eebb73ebbdcb67bb9a1482
next-fire-starter <next.fire.starter@gmail.com>
2021-05-23 00:11:55 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require("./app");
 require('dotenv').config();
 
 app.listen(process.env.EXPRESS_PORT, () => {
-  console.log(`App running on Express.`)
+  console.log(`App running on Express.`) 
 })
